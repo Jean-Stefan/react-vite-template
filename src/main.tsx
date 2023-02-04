011871c5a6030a231d3197e009e7e1b5eacb66a1
Jean-Stefan <jeanss12@gmail.com>
2023-02-04 15:54:11 -0300

    chore: updated main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container as HTMLDivElement);
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>,
+);
